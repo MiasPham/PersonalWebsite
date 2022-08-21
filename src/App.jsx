d3e2cad8d62d5cd54aeccfe190b0d83de8080f67
MiaPham2520 <mia2520@uw.edu>
2022-08-21 08:31:47 -0700

    About

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
-import Contact from "./components/contact/Contact";
 import Experience from "./components/experience/Experience";
+import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 
 
@@ -13,8 +13,8 @@ const App = () => {
       <Header />
       <Nav />
       <About />
-      <Contact />
       <Experience />
+      <Contact />
       <Footer />
     </div>
   )
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,31 @@
-import React from "react";
-import "./about.css";
+import React from 'react';
+import './about.css';
+import about from '../../assets/about.jpeg'
 
 const About = () => {
   return (
     <section id='about'>
-      About
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+    <div className="container about__container">
+      <div className="about__me">
+        <div className="about__me-img">
+          <img src={about} alt="me" />
+        </div>
+      </div>
+
+      <div className="about__content">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam suscipit nisi quis metus
+        euismod euismod. Donec sagittis leo nec sapien feugiat sollicitudin. Nunc condimentum
+        dignissim leo, sed maximus sem ullamcorper non. Maecenas eu ullamcorper ipsum.
+      </p>
+
+    <a href="#contact" className='btn btn-primary'>Contact Me</a>
+    </div>
+    </div>
+
     </section>
   )
 }
