8cbd942689f7e779aac8d2c2a025025a7966059d
MiaPham2520 <mia2520@uw.edu>
2022-08-21 22:51:17 -0700

    Contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,37 @@
-import React from "react";
-import "./contact.css";
+import React,  { useRef }  from 'react';
+import './contact.css';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_co87i1g', 'template_nxszxza', form.current, 'iI_mvsUDWi57qmWSU')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+    e.target.reset()
+  };
+
   return (
-    <section id="contact">contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="10" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
