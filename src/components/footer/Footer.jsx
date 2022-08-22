3a8461f688dce81fe650f8e2994e89986a33b639
MiaPham2520 <mia2520@uw.edu>
2022-08-21 23:40:08 -0700

    Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,29 @@
-import React from "react";
-import "./footer.css"
+import React from 'react'
+import './footer.css'
+import {SiLinkedin} from 'react-icons/si'
+import {SiGithub} from 'react-icons/si'
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+      <h1>Mia Pham</h1>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experiences</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/mia-pham-2520"><SiLinkedin/></a>
+        <a href="https://github.com/MiasPham"><SiGithub/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>Portion of this page is ©Mia Pham. Other attributions cited in
+      document or source page.</small>
+      </div>
+    </footer>
   )
 }
 
