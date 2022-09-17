663b3a0e5030ee270db2e7c9aa7b6805977df7e9
MiaPham2520 <mia2520@uw.edu>
2022-09-17 10:36:46 -0700

    Experience Updated

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -9,28 +9,28 @@ const Experience = () => {
 
       <div className="container experience__container">
         <div className="experience__content">
-          <h3>Title</h3>
-          <h4 className='text-light'>Position:</h4>
+          <h3>Scandanavian Software Park</h3>
+          <h4 className='text-light'>Position: Intership</h4>
             <article className='experience__details'>
               <ul>
-                <li>Description 1</li>
-                <li>Description 2</li>
-                <li>Description 3</li>
+              <li>Developed bookings and time schedule for different universities</li>
+              <li>Update websites based on client requirements</li>
+              <li>Optimized websites by testing out codes</li>
               </ul>
               <p><b>Languages:</b> HTML, CSS, Javascript, React</p>
             </article>
         </div>
 
         <div className="experience__content">
-          <h3>Title</h3>
-          <h4 className='text-light'>Position:</h4>
+          <h3>Rainy Dawg Daze Radio</h3>
+          <h4 className='text-light'>Position: Technology Manager</h4>
           <article className='experience__details'>
             <ul>
-              <li>Description 1</li>
-              <li>Description 2</li>
-              <li>Description 3</li>
+              <li>Maintain and develop ideas to update the station’s website</li>
+              <li>Upkeep the Rainy Dawg server</li>
+              <li>Run and coordinate sound for live, in studio, and campus events</li>
             </ul>
-            <p><b>Languages:</b> HTML, CSS, Javascript, React</p>
+            <p><b>Languages:</b> HTML, CSS, Javascript</p>
           </article>
         </div>
       </div>
